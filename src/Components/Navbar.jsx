dc471c4bfa711f26ca7a3b8819d4dd73c14f88e7
mamta1138 <ghimire.mamta98@gmail.com>
2024-02-26 13:22:48 +0545

    Please trust me sir

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ import {
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/Mamta Ghimire.jpg';
+import Logo from '../assets/Mamta Ghimire.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -3,18 +3,11 @@ import React from 'react';
 const Work = () => {
   return (
     <div name='work' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-    <div className='pb-8'>
-    <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Work</p>
-    <p className='text-gray-300 py-4 '>"Cultivating a rich background in diverse work environments,
-     I've honed my expertise across various industries and roles. From spearheading innovative projects
-      in tech startups to navigating complex challenges within corporate landscapes, my journey encompasses
-       a spectrum of experiences. With a commitment to excellence and a passion for problem-solving,
-        I've collaborated with teams of all sizes, from agile startups to multinational enterprises,
-         delivering impactful solutions that drive growth and success. My work ethos is centered on continuous learning,
-          adaptability, and a relentless pursuit of excellence, ensuring every endeavor surpasses expectations
-           and leaves a lasting impact."</p>
-</div>
-</div>
+      <div className='text-center max-w-4xl px-8'> {/* Added max-width and padding */}
+        <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Work</p>
+        <p className='text-gray-300 py-4'>"Cultivating a rich background in diverse work environments, I've honed my expertise across various industries and roles. From spearheading innovative projects in tech startups to navigating complex challenges within corporate landscapes, my journey encompasses a spectrum of experiences. With a commitment to excellence and a passion for problem-solving, I've collaborated with teams of all sizes, from agile startups to multinational enterprises, delivering impactful solutions that drive growth and success. My work ethos is centered on continuous learning, adaptability, and a relentless pursuit of excellence, ensuring every endeavor surpasses expectations and leaves a lasting impact."</p>
+      </div>
+    </div>
   );
 };
 
